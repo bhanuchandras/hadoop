e97c04f1ff3b6cfa8e949df38a53f59e9778061d
bhanuchandras <bhanuandchandra@gmail.com>
2018-12-09 09:45:58 +0000

    remote-exec success

--- master.tf
+++ master.tf
@@ -15,6 +15,13 @@ resource "google_compute_instance" "hadoop-m" {
     }
   }
   provisioner "remote-exec" {
+    connection {
+      type    = "ssh"
+      user    = "bhanuchandra_sabbavarapu"
+      timeout = "120s"
+      agent       = false
+      private_key = "${file("/home/bhanuchandra_sabbavarapu/.ssh/google_compute_engine")}"
+    }
     inline = [
       "sudo yum install -y ansible"
     ]
