697bcd735efc6a641db95e97a29118b695b793ab
Bhanu <bhanuandchandra@gmail.com>
2024-03-06 18:31:36 +0530

    Update master.tf with rhel 8.8

--- master.tf
+++ master.tf
@@ -1,12 +1,12 @@
 resource "google_compute_instance" "hadoop-m" {
-  project      = "datacloud-20220501"
+  project      = "dl-9953189580"
   machine_type = "e2-standard-4"
   zone         = "us-west2-a"
   name = "hadoop-master"
  
   boot_disk {
     initialize_params {
-      image = "rhel-cloud/rhel-8"
+      image = "rhel-sap-cloud/rhel-8-8-sap-ha"
       size  = 50
     }
   }
